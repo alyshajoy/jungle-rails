9da9a2aba733ab108cb42ce8f285a40dece8a378
Alysha Huot <alyshajhuot@gmail.com>
2024-04-09 15:00:41 -0600

    add product/category counts to admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['HTTP_AUTH_USERNAME'], password: ENV['HTTP_AUTH_PASS']
 
   def show
+    @products_count = Product.count
+    @categories_count = Category.count
   end
 end
