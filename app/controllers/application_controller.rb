a68cec907f0558ed228e8fb7a2945fc2042d1312
Alysha Huot <alyshajhuot@gmail.com>
2024-04-13 11:57:18 -0600

    add links for user to login/logout/register in nav

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,19 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    current_user.present?
+  end
+
+  def require_user
+    redirect_to login_path, alert: "You must be logged in to access this page." unless logged_in?
+  end
   
   protect_from_forgery with: :exception
 
