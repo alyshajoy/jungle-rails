b896240f5649f61ddbe29339a911063145914a9f
Alysha Huot <alyshajhuot@gmail.com>
2024-04-12 23:03:21 -0600

    create admin categories page with a list of all categories

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,28 @@
+class Admin::CategoriesController < ApplicationController
+
+  http_basic_authenticate_with name: ENV['HTTP_AUTH_USERNAME'], password: ENV['HTTP_AUTH_PASS']
+
+  def index
+    @categories = Category.order(:name).all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @category = Category.find params[:id]
+    @category.destroy
+    redirect_to [:admin, :categories], notice: 'Category deleted!'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    resources :categories
+  end
   get '/about', to: 'pages#about'
   root to: 'products#index'
 
