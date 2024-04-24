6e580459ddfab529ef16084f6c612517971f3138
Alysha Huot <alyshajhuot@gmail.com>
2024-04-23 23:00:53 -0600

    update login functionality and add corresponding tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,10 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 8 }
 
+  def self.authenticate_with_credentials(email, password)
+    normalized_email = email.strip.downcase
+    user = User.find_by(email: normalized_email)
+    user && user.authenticate(password) ? user : nil
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,64 +2,86 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  it 'is valid when all fields are included, email is unique, and passwords match' do
-    user = User.new(first_name: "Alysha", last_name: "Huot", email: "alyshajhuot@gmail.com", password: 'password123', password_confirmation: 'password123')
-    expect(user).to be_valid
+  describe 'Validations' do
+
+    it 'is valid when all fields are included, email is unique, and passwords match' do
+      user = User.new(first_name: "Alysha", last_name: "Huot", email: "alyshajhuot@gmail.com", password: 'password123', password_confirmation: 'password123')
+      expect(user).to be_valid
+    end
+
+    it 'is not valid when passwords do not match' do
+      user = User.new(first_name: "Alysha", last_name: "Huot", email: "alyshajhuot@gmail.com", password: 'password123', password_confirmation: 'password1234')
+      expect(user).not_to be_valid
+    end
+
+    it 'is not valid without a first name' do
+      user = User.new(last_name: "Huot", email: "alyshajhuot@gmail.com", password: 'password123', password_confirmation: 'password123')
+      expect(user).not_to be_valid
+    end
+
+    it 'is not valid without a last name' do
+      user = User.new(first_name: "Alysha", email: "alyshajhuot@gmail.com", password: 'password123', password_confirmation: 'password123')
+      expect(user).not_to be_valid
+    end
+
+    it 'is not valid without an email' do
+      user = User.new(first_name: "Alysha", last_name: "Huot", password: 'password123', password_confirmation: 'password123')
+      expect(user).not_to be_valid
+    end
+
+    it 'is not valid if email is not unique' do
+      user = User.create(first_name: "Alysha", last_name: "Huot", email: "alyshajhuot@gmail.com", password: 'password123', password_confirmation: 'password123')
+      expect(user).to be_valid
+
+      user2 = User.new(first_name: "Alysha", last_name: "Huot", email: "alyshajhuot@gmail.com", password: 'password123', password_confirmation: 'password123')
+      user2.valid?
+      expect(user2).not_to be_valid
+      expect(user2.errors[:email]).to include("has already been taken")
+    end
+
+    it 'is not valid if email is not unique, but has different case' do
+      user = User.create(first_name: "Alysha", last_name: "Huot", email: "alyshajhuot@gmail.com", password: 'password123', password_confirmation: 'password123')
+      expect(user).to be_valid
+
+      user2 = User.new(first_name: "Alysha", last_name: "Huot", email: "ALYSHAJHUOT@gmail.com", password: 'password123', password_confirmation: 'password123')
+      user2.valid?
+      expect(user2).not_to be_valid
+      expect(user2.errors[:email]).to include("has already been taken")
+    end
+
+    it 'is not valid if the password is shorter than 8 characters' do
+      user = User.new(first_name: "Alysha", last_name: "Huot", email: "alyshajhuot@gmail.com", password: '1234', password_confirmation: '1234')
+      expect(user).not_to be_valid
+    end
+
   end
 
-  it 'is not valid when passwords do not match' do
-    user = User.new(first_name: "Alysha", last_name: "Huot", email: "alyshajhuot@gmail.com", password: 'password123', password_confirmation: 'password1234')
-    expect(user).not_to be_valid
-  end
+  describe '.authenticate_with_credentials' do
 
-  it 'is not valid without a first name' do
-    user = User.new(last_name: "Huot", email: "alyshajhuot@gmail.com", password: 'password123', password_confirmation: 'password123')
-    expect(user).not_to be_valid
-  end
+    before do
+      User.create!(first_name: "Test", last_name: "User", email: "example@example.com", password: "password123", password_confirmation: "password123")
+    end
 
-  it 'is not valid without a last name' do
-    user = User.new(first_name: "Alysha", email: "alyshajhuot@gmail.com", password: 'password123', password_confirmation: 'password123')
-    expect(user).not_to be_valid
-  end
+    it 'authenticates correctly with valid credentials' do
+      user = User.authenticate_with_credentials('example@example.com', 'password123')
+      expect(user).not_to be_nil
+    end
 
-  it 'is not valid without an email' do
-    user = User.new(first_name: "Alysha", last_name: "Huot", password: 'password123', password_confirmation: 'password123')
-    expect(user).not_to be_valid
-  end
+    it 'does not authenticate with incorrect password' do
+      user = User.authenticate_with_credentials('example@example.com', '234')
+      expect(user).to be_nil
+    end
+    
+    it 'authenticates correctly if extra spaces are included around email' do
+      user = User.authenticate_with_credentials('    example@example.com ', 'password123')
+      expect(user).not_to be_nil
+    end
 
-  it 'is not valid if email is not unique' do
-    user = User.create(first_name: "Alysha", last_name: "Huot", email: "alyshajhuot@gmail.com", password: 'password123', password_confirmation: 'password123')
-    expect(user).to be_valid
-
-    user2 = User.new(first_name: "Alysha", last_name: "Huot", email: "alyshajhuot@gmail.com", password: 'password123', password_confirmation: 'password123')
-    user2.valid?
-    expect(user2).not_to be_valid
-    expect(user2.errors[:email]).to include("has already been taken")
-  end
-
-  it 'is not valid if email is not unique, but has different case' do
-    user = User.create(first_name: "Alysha", last_name: "Huot", email: "alyshajhuot@gmail.com", password: 'password123', password_confirmation: 'password123')
-    expect(user).to be_valid
-
-    user2 = User.new(first_name: "Alysha", last_name: "Huot", email: "ALYSHAJHUOT@gmail.com", password: 'password123', password_confirmation: 'password123')
-    user2.valid?
-    expect(user2).not_to be_valid
-    expect(user2.errors[:email]).to include("has already been taken")
-  end
-
-  it 'is not valid if the password is shorter than 8 characters' do
-    user = User.new(first_name: "Alysha", last_name: "Huot", email: "alyshajhuot@gmail.com", password: '1234', password_confirmation: '1234')
-    expect(user).not_to be_valid
+    it 'authenticates correctly if email is typed in a different case' do
+      user = User.authenticate_with_credentials('EXAMPLE@example.com', 'password123')
+      expect(user).not_to be_nil
+    end
+    
   end
 
 end
-
-
-# create_table "users", force: :cascade do |t|
-#   t.string "first_name"
-#   t.string "last_name"
-#   t.string "email"
-#   t.string "password_digest"
-#   t.datetime "created_at", precision: 6, null: false
-#   t.datetime "updated_at", precision: 6, null: false
-# end
